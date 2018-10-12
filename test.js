2648a5cd236a6a6018b7da3355f9f8ddca3563f4
widmann_eric <erwidman@mtu.edu>
2018-10-12 17:55:54 -0400

    dbInterface first step

--- test.js
+++ test.js
@@ -3,16 +3,59 @@
 
 
 
-
- const oneEqualsone = require('./private/anExampleTest.js').oneEqualsone;
- describe("Basic Test for anExampleTest:",function(){
-    it("-One should in fact equal one",function(){
-        expect(oneEqualsone()).to.be.true;
+//db interface test
+const db = require('./private/dbInterface.js');
+describe("Working on dbInterface.js",function(){
+    
+   
+    it("-Testing createUser",async function(){
+        db.removeUser("zigzig");
+        let result = await db.createUser("zigzig","1234");
+        assert.equal(result,true);
     });
-    it("-2 + 2 should equal four",function(){
-        expect(2+2).to.equal(4);
-    })
-    it("-properties should exist",function(){
-        assert.property({aProperty:1},'aProperty');
-    })
- });
+
+    it("-Testing nonunique username createUser",async function(){
+        let result = await db.createUser("zigzig","1234");
+        assert.notEqual(result,true);
+    });
+
+    it("-Testing login functions",async function(){
+        let result = await db.login("zigzig","1234");
+        assert.equal(result,true);
+    });
+
+    it("-Testing failed login",async function(){
+        let result = await db.login("zigzig","aklsjdf;lk");
+        assert.notEqual(result,true);
+    });
+
+    it("-Testing ipBlacklist addToBlacklist", async function(){
+        let result = await db.addToBlacklist("255.255.255.255");
+        assert.equal(result,true);
+    });
+
+    it("-Testing ipBlacklist duplicate add", async function(){
+        let result = await db.addToBlacklist("255.255.255.255");
+        assert.notEqual(result,true);
+    });
+
+    it("-Testing ipBlacklist removeFromBlacklist", async function(){
+        let result = await db.removeFromBlacklist("255.255.255.255");
+        assert.equal(result,true);
+    });
+
+
+    it("-Testing create article, lock article, and update article",async function(){
+        db.createArticle(200,"Another article","sl");
+        db.createArticle(2034,"Still article","sl");
+        let artid = await db.createArticle(44,"Test Article","This is a test. This is a test");
+        assert.equal(artid>0,true);
+        let result = await db.lockArticle(artid);
+        assert.equal(result,true);
+        result = await db.updateArticle(1000,artid,"This is the replaced test");
+        assert.equal(result,true);
+    });
+
+
+
+});

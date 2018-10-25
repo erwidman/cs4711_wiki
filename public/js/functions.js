3c785bc49eede75fde86887a6e5d4ee2e1cfa35a
Keith <kwatkins@mtu.edu>
2018-10-25 19:59:15 -0400

    login user

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,9 +1,9 @@
-$('#create-user').on('click', function () {
-    var args = {
-        username: 'keith' + new Date().getTime().toString().substr(5,13),
-        password: 'test',
+$('#create-user-button').on('click', function () {//when an element with id="create-user-button" is clicked
+    var args = {//a dictionary object
+        username: $('#create-user-email').val(),// val of the input with id="create-user-email"
+        password: $('#create-user-password').val(),
         successCallback: function () {
-            alert("success! created user")
+            alert("success! created user")//function to do when complete(Like hiding loading icon)
         }
     };
     console.log('creating user: ', args);
@@ -13,18 +13,59 @@ $('#create-user').on('click', function () {
 /**
  *
  * @param args {object}
- * @param [args.username] - users username, presumably email
- * @param [args.password] - users password
- * @param [args.successCallback] - functions used when ajax is successful
- * @param [args.failureCallback] - functions used when ajax fails
+ * @param [args.username] - users username, presumably email. No spaces
+ * @param [args.password] - users password no spaces
+ * @callback [args.successCallback] - functions used when ajax is successful
+ * @callback [args.failureCallback] - functions used when ajax fails
  */
 function createUser(args) {
+    if(!args.username || !args.password){
+        args.failureCallback || function () {
+            alert("error in createUser" + JSON.stringify(args))
+        }()
+    }
     $.ajax({
         url: '/request?command=createUser&0='+args.username,
         async: true,
         success: args.successCallback,
         error: args.failureCallback || function () {
             alert("error in createUser")
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('authorization', args.password);
+        }
+    })
+}
+
+
+$('#login-user-button').on('click', function () {
+    var args = {
+        username: $('#login-user-email').val(),
+        password: $('#login-user-password').val(),
+        successCallback: function () {
+            alert("success! created user")
+        }
+    };
+    console.log('login user: ', args);
+    login(args)
+});
+
+
+/**
+ *
+ * @param args {object}
+ * @param [args.username] - users username, presumably email
+ * @param [args.password] - users password
+ * @param [args.successCallback] - functions used when ajax is successful
+ * @param [args.failureCallback] - functions used when ajax fails
+ */
+function login(args) {
+    $.ajax({
+        url: '/request?command=login&0='+args.username,
+        async: true,
+        success: args.successCallback,
+        error: args.failureCallback || function () {
+            alert("error in login User")
         },
         beforeSend: function (xhr) {
             xhr.setRequestHeader('authorization', args.password);

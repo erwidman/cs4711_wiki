c82c743e15cec5871d79bdb6eb006ee2407ae36d
widmann_eric <erwidman@mtu.edu>
2018-10-25 12:46:35 -0400

    test

--- test.js
+++ test.js
@@ -188,7 +188,6 @@ describe("Working appBinding.js",function(){
 
     it("-getAllImages",async function(){
         let res = await createRequest('getAllImages',[],"");
-        console.log(res.data);
         expect(res.data[0]).to.have.property('imageid');
     });
 
